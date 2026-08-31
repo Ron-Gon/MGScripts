9c610085772342eb727f1b483a012244d3da55d8
Ron-Gon <80388694+Ron-Gon@users.noreply.github.com>
2026-09-01 01:12:17 +0800

    Update Bonk.user.js

--- Bonk.user.js
+++ Bonk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bonk Bonk
 // @namespace    Violentmonkey
-// @version      1.0.6
+// @version      1.0.7
 // @description  Floating overlay with buttons, buttons simulates key presses
 // @author       AWON
 // @match        https://magicgarden.gg/r/*
